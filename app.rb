1cf8b02026d17678b985c2bc316f308e04dba542
Osman Khan <osman_khan1987@hotmail.com>
2023-06-05 15:22:21 +0100

    register

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative 'lib/registration'
+
 
 class Application < Sinatra::Base
   configure :development do
@@ -7,6 +9,27 @@ class Application < Sinatra::Base
   end
 
   get '/' do
-    return erb(:index)
+    erb :index
   end
+
+  get '/register' do
+    erb :register
+  end
+
+  post '/register' do
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+
+    registration = Registration.new(name, email, password)
+    result = registration.register
+
+    # Do something with the result, such as displaying a success message or handling errors
+
+    # Return a response
+    # Example: "User registration successful!"
+  end
+
+  # Other routes and methods
+
 end

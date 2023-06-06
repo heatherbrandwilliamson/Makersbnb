78f620b779d77a9a1f0c883bd4ea29db7a0ab4f3
Sean Peters <sean@snptrs.dev>
2023-06-06 08:31:07 +0100

    Update schema and add seeds

---
+++ spec/seeds.sql
@@ -0,0 +1,5 @@
+TRUNCATE TABLE users, properties, users_properties RESTART IDENTITY; -- replace with your own table name.
+
+INSERT INTO users (name, email, phone_number, password) VALUES ('Jane', 'jane@example.com', '07700123456', 'pass');
+
+INSERT INTO properties (property_name, price, location, description, user_id) VALUES ('Beach House', 100, 'Cornwall', 'Lovely sea views.', 1);

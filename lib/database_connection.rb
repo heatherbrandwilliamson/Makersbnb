8e36fcb7d1b72cdf09845fb2f697bb1d4ba2e890
Sean Peters <sean@snptrs.dev>
2023-06-09 05:59:28 +0100

    Revert "Update database connection for deploy"
    This reverts commit 0909cc3ca056f9da30653e2dc660c2025d9f4aba.

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -12,12 +12,6 @@ class DatabaseConnection
   using Rainbow
 
   def self.connect
-  
-    if ENV['DATABASE_URL'] != nil
-      @connection = PG.connect(ENV['DATABASE_URL'])
-      return
-    end
-  
     if ENV['ENV'] == 'test'
       ENV['DB_NAME'] = 'makersbnb_test'
     else
